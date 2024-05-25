4932a36bab5eb602321a7277d67e5550551b95a5
Arthur Hung <asssde2002@gmail.com>
2024-05-25 15:23:00 +0800

    Optimize space with bitarray package

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,19 +1,20 @@
 import hashlib
+from bitarray import bitarray
 
 
 class BloomFilter:
     def __init__(self, bit_size: int, hash_function_list: list):
         self.bit_size = bit_size
-        self.bit_array = [False]*self.bit_size
+        self.bit_array = bitarray(self.bit_size)
         self.hash_function_list = hash_function_list
 
-    def _get_hash_index(self, hash_func, data):
+    def _get_hash_index(self, hash_func, data: str):
         return int(hash_func(data.encode()).hexdigest(), 16) % self.bit_size
     
     def add_data(self, data: str):
         for hash_func in self.hash_function_list:
             index = self._get_hash_index(hash_func, data)
-            self.bit_array[index] = True
+            self.bit_array[index] = 1
 
     def check_data_existed(self, data: str):
         result = True
@@ -22,6 +23,9 @@ class BloomFilter:
             result &= self.bit_array[index]
         
         return result
+    
+    def reset(self):
+        self.bit_array.setall(0)
     
 
 if __name__ == '__main__':
